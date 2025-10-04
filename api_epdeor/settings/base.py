47188caebc18136310ef127d3505739b1d37da23
Daniel Rojas <danielrojas.alvaro2@gmail.com>
2025-10-04 19:46:05 -0400

    chore(base): agregada configuracion para el CORS

--- api_epdeor/settings/base.py
+++ api_epdeor/settings/base.py
@@ -43,6 +43,7 @@ LOCAL_APPS = [
 ]
 
 THIRD_APPS = [
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -59,6 +60,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
